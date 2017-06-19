5f6ed224571cdb29dc8d0bee4a7af9193c67fa91
Akila Adithya <akilaa@JKCSWORLD.INT>
2017-06-19 14:40:03 +0530

    Initial Commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation =React.createClass({
+  render:function(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Timer App</li>
+            <li><IndexLink to="/" activeClassName="active">Timer</IndexLink></li>
+            <li><Link to="/about" activeClassName="active">Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className='menu'>
+            <li className="menu-text">
+              <span>Created by </span>
+              <a href="#">Akila Adithya</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+module.exports=Navigation;
